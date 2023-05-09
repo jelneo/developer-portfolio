53d96f824a6fcc0ebd45db343a35aa67ec582e3f
Jelena Neo <neohuilingjelena@gmail.com>
2023-05-09 11:01:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: false,
+  assetPrefix: isProd ? '/developer-portfolio/' : '',
   images: {
+    unoptimized: true,
     domains: ['avatars.githubusercontent.com'],
   },
 };
