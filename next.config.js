b3bad1237384db9bdf76978375d89639381ce0a4
Jelena Neo <jelena.neo@shopee.com>
2023-05-09 11:56:38 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-const repo = 'developer-portfolio'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}` 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
+  reactStrictMode: false,
   assetPrefix: assetPrefix,
   basePath: basePath,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  }
 }
